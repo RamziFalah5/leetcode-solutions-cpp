d5f6f065dfd4fa6a59ef86ad7321ab56173d6dee
Ramzi Abu Falah <1230594@student.birzeit.edu>
2026-04-08 22:06:21 +0300

    Update 657. Robot Return to Origin.cpp

--- easy/657. Robot Return to Origin.cpp
+++ easy/657. Robot Return to Origin.cpp
@@ -1,1 +1,15 @@
-easy
+//https://leetcode.com/problems/robot-return-to-origin/
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int n = moves.size();
+        vector<int> v(26, 0);
+
+        for (int i = 0; i < n; i++) {
+            v[moves[i] - 'A']++;
+        }
+
+        return v[20] == v[3] && v[11] == v[17];
+    }
+};
