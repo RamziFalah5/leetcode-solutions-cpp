50b527744b7e89c9fa301e653d695ce84fde9e4e
Ramzi Abu Falah <1230594@student.birzeit.edu>
2026-04-26 00:52:01 +0300

    Refactor validDigit function for clarity and efficiency

--- 1.Easy/3908.ValidDigitNumber.cpp
+++ 1.Easy/3908.ValidDigitNumber.cpp
@@ -3,21 +3,9 @@
 class Solution {
 public:
     bool validDigit(int n, int x) {
-        bool is1 = false;
-        bool is2 = false;
-        while (n != 0) {
-            if (n / 10 == 0) {
-                if (n % 10 != x)
-                    is2 = true;
-            }
-            int num = n % 10;
-            if (num == x)
-                is1 = true;
-            n /= 10;
-        }
-        if (is1 == true && is2 == true)
-            return true;
-        else
-            return false;
+        string s = to_string(n);
+        char digit = x + '0';
+
+        return s.find(digit) != string::npos && s[0] != digit;
     }
 };
